c05cee8eb5e7151e518b17c2259f96c64eb699fd
Cowportal <73531403+Cowportal@users.noreply.github.com>
2020-11-04 14:21:56 +0200

    Add project files.

---
+++ Inheritance_exercise/TwoDimensionalShapes/Triangle.cs
@@ -0,0 +1,13 @@
+﻿using Inheritance_exercise.ThreeDimensionalShapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_exercise.TwoDimensionalShapes
+{
+    class Triangle:TwoDimensionalShapes
+    {
+    }
+}
